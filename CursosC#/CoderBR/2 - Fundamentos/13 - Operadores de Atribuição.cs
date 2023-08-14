2e6277942123225c7768a52c01860fadebd3ff10
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-08-14 15:13:01 -0300

    operador de atribuição

--- CursosC#/CoderBR/2 - Fundamentos/13 - Operadores de Atribuição.cs
+++ CursosC#/CoderBR/2 - Fundamentos/13 - Operadores de Atribuição.cs
@@ -87,7 +87,7 @@ string city = null;
 string defaultCity = "New York";
 string selectedCity = city ?? defaultCity; // 'selectedCity' receberá "New York" porque 'city' é null
 
-Esses operadores de atribuição são ferramentas poderosas para tornar seu código mais conciso e legível, permitindo que você execute operações de atribuição e cálculos em uma única linha de código. */
+Esses operadores de atribuição são ferramentas poderosas para tornar seu código mais conciso e legível, permitindo que você execute operações de atribuição e cálculos em uma única linha de código. 
 
 
 
