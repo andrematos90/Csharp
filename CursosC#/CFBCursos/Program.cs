3c4835cba0d31664a905c5dad4b571b5fa353ca0
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-08-18 14:05:38 -0300

    Aula 01

--- CursosC#/CFBCursos/Program.cs
+++ CursosC#/CFBCursos/Program.cs
@@ -7,7 +7,7 @@ namespace CursoCSharp {
     class Program {
         static void Main(string[] args) {
             var central = new Central(new Dictionary<string, Action>() {
-                {"Primeiro Programa - Fundamentos", Aula01.Executar},
+                {"Aula 01", Aula01.Executar},
             });
 
             central.SelecionarEExecutar();
