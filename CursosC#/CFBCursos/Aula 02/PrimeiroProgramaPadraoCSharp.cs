1c7275fff952462a1c44105a34c3a49f8f8f067a
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-08-18 14:41:21 -0300

    Aula 02 -  PrimeiroProgramaPadraodotNet

--- CursosC#/CFBCursos/Aula 02/PrimeiroProgramaPadraoCSharp.cs
+++ CursosC#/CFBCursos/Aula 02/PrimeiroProgramaPadraoCSharp.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CFBCursos.Aula02
-{
-     class PrimeiroProgramaPadraoCSharp
-    {
-        public static void Executar()
-        {
+/*
 
-        }
-    }
-}
+class PrimeiroProgramaPadraodotNet
+  {
+     static void Main(string[] args)
+     {
+          Console.WriteLine("Hello World!!!!!!!!!!!");
+     }
+  }
+
+
+ Aqui está uma explicação passo a passo do seu código:
+
+class PrimeiroProgramaPadraodotNet: Isso declara uma nova classe chamada PrimeiroProgramaPadraodotNet.
+
+static void Main(string[] args): Este é o método principal (Main) do seu programa. É o ponto de entrada onde a execução do programa começa. O método é declarado como estático (static), o que significa que ele pertence à classe em vez de uma instância específica da classe.
+
+Console.WriteLine("Hello World!!!!!!!!!!!"): Esta linha de código usa a classe Console para imprimir a string "Hello World!!!!!!!!!!!" no console. O método WriteLine adiciona uma nova linha após a string, o que resultará na impressão da mensagem no console seguida por uma quebra de linha.
+
+Em geral, esse código cria uma classe, define um método Main, usa a classe Console para imprimir uma mensagem no console e representa uma maneira válida de criar um programa de "Hello World" em C# usando o padrão .NET.
+ 
+ Em resumo, a principal diferença entre os dois exemplos é o uso de um namespace no segundo exemplo (.NET), o que é mais comum em projetos maiores para melhor organização e prevenção de conflitos de nomes.
+ 
+
+ */
--- CursosC#/CFBCursos/Program.cs
+++ CursosC#/CFBCursos/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using CFBCursos.Aula01;
-using CFBCursos.Aula02;
+//using CFBCursos.Aula02;
 
 namespace CursoCSharp {
     class Program {
         static void Main(string[] args) {
             var central = new Central(new Dictionary<string, Action>() {
                 {"Aula 01 - Primeiro Programa Padrão C# Puro", PrimeiroProgramaPadraoCsharpPuro.Executar},
-                {"Aula 02 - Primeiro Programa Padrao C#", PrimeiroProgramaPadraoCSharp.Executar }
+               // {"Aula 02 - Primeiro Programa Padrao .Net", PrimeiroProgramaPadraodotNet.Executar }
             });
 
             central.SelecionarEExecutar();
