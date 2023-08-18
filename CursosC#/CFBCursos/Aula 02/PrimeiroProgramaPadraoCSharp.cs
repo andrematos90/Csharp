29ae697d9918cfea41996750b7f787a22c097774
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-08-18 14:22:31 -0300

    "Aula 01", PrimeiroProgramaPadraoCsharpPuro

---
+++ CursosC#/CFBCursos/Aula 02/PrimeiroProgramaPadraoCSharp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFBCursos.Aula02
+{
+     class PrimeiroProgramaPadraoCSharp
+    {
+        public static void Executar()
+        {
+
+        }
+    }
+}
--- CursosC#/CFBCursos/Program.cs
+++ CursosC#/CFBCursos/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-
 using CFBCursos.Aula01;
+using CFBCursos.Aula02;
 
 namespace CursoCSharp {
     class Program {
         static void Main(string[] args) {
             var central = new Central(new Dictionary<string, Action>() {
                 {"Aula 01", Aula01.Executar},
+                {"Aula 02 - Primeiro Programa Padrao C#", PrimeiroProgramaPadraoCSharp.Executar }
             });
 
             central.SelecionarEExecutar();
