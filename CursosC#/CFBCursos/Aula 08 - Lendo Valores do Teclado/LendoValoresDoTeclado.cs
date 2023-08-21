22f653475fc4707a4b800edf475ddd57b86e443e
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-08-21 15:09:02 -0300

    "Aula 08 - Lendo Valores do Teclado

--- CursosC#/CFBCursos/Aula 08 - Lendo Valores do Teclado/LendoValoresDoTeclado.cs
+++ CursosC#/CFBCursos/Aula 08 - Lendo Valores do Teclado/LendoValoresDoTeclado.cs
@@ -12,9 +12,9 @@ namespace CFBCursos.Aula08
             int v1, v2, soma;
 
             Console.Write("Digite o primeiro valor: ");
-          v1 = int.Parse(Console.ReadLine());
+            v1 = int.Parse(Console.ReadLine());
             Console.Write("Digiteo segundo valor: ");
-           v2 = Convert.ToInt32(Console.ReadLine());
+            v2 = Convert.ToInt32(Console.ReadLine());
             soma = v1 + v2;
             Console.Write($"Soma de {v1} + {v2} = {soma}");
 
