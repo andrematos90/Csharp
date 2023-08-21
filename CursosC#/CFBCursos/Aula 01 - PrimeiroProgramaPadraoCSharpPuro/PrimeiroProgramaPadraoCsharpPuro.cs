fc007a5c04b5a99035d158a24e03026220c12d8b
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-08-21 09:31:16 -0300

    Aula 01 - Primeiro programa C# puro

--- CursosC#/CFBCursos/Aula 01 - PrimeiroProgramaPadraoCSharpPuro/PrimeiroProgramaPadraoCsharpPuro.cs
+++ CursosC#/CFBCursos/Aula 01 - PrimeiroProgramaPadraoCSharpPuro/PrimeiroProgramaPadraoCsharpPuro.cs
@@ -4,14 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace CFBCursos.Aula01
 {
     class PrimeiroProgramaPadraoCsharpPuro
     {
         public static void Executar()
         {
-            Console.WriteLine("Hello World!!!!");
+            Console.WriteLine("Hello World!!!!!");
         }
     }
 }
