a0b3967c1f6d2a03796c27adb333bebf7b364598
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-08-25 16:15:17 -0300

    Aula 10 - Enumeradores (Enum)

---
+++ CursosC#/CFBCursos/Aula 10 - Enumeradores(Enum)/Enum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CFBCursos.Aula10
+{
+    class Enumeradores
+    {
+        enum DiasSemana { Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado };
+        public static void Executar()
+        {
+            DiasSemana ds = (DiasSemana)3;
+
+            DiasSemana ds2 = (DiasSemana.Quarta);
+
+            Console.WriteLine(ds);
+            Console.WriteLine(ds2);
+
+
+            int ds3 = (int)DiasSemana.Quarta;
+
+            Console.WriteLine(ds3); 
+
+            
+        }
+    }
+}
--- CursosC#/CFBCursos/Program.cs
+++ CursosC#/CFBCursos/Program.cs
@@ -9,6 +9,7 @@ using CFBCursos.Aula06;
 using CFBCursos.Aula07;
 using CFBCursos.Aula08;
 using CFBCursos.Aula09;
+using CFBCursos.Aula10;
 
 
 namespace CursoCSharp
@@ -27,6 +28,7 @@ namespace CursoCSharp
                 {"Aula 07 - Constantes",  Constantes.Executar },
                 {"Aula 08 - Lendo Valores do Teclado",  LendoValoresDoTeclado.Executar },
                 {"Aula 09 - Operações de Bitwise",  OperadoresDeBitwise.Executar },
+                {"Aula 10 - Enumeradores (Enum)", Enumeradores.Executar },
 
             });
 
