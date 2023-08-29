3668ecbdb45884717f87aa31b969f3a1b481acff
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-08-29 11:59:11 -0300

    Aula 13 - Condicional  If Else

--- CursosC#/CFBCursos/Aula 13 - Condicional If-Else/CondicionalIfElse.cs
+++ CursosC#/CFBCursos/Aula 13 - Condicional If-Else/CondicionalIfElse.cs
@@ -10,6 +10,38 @@ namespace CFBCursos.Aula13
     {
         public static void Executar()
         {
+            float n1, n2, n3, n4;
+            n1 = n2 = n3 = n4 = 0;
+            string resultado = "";
+
+            Console.Write("Digite o valor da primera nota: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor da segunda nota: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor da terceira nota: ");
+            n3 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor da quarta nota: ");
+            n4 = int.Parse(Console.ReadLine());
+
+            float media = (n1 + n1 + n3 + n4) / 4;
+
+            if (media >= 7)
+            {
+                resultado = "Apovado";
+            }
+            else if( media <= 6 && media >= 4)
+            {
+                resultado = "Recuperação";
+            }
+            else
+            {
+                resultado = "Reprovado";
+            }
+
+            Console.WriteLine("média {0}    situação: {1}", media, resultado);
 
         }
     }
