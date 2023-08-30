815266a92c968bd9180a7b36d6262d43c92c1f2e
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-08-30 14:27:36 -0300

    Aula 18 - Arrays bidimensionais (matrizes)

--- CursosC#/CFBCursos/Aula 18 - Arrays Bidimensionais (matrizes)/ArraysMatrizes.cs
+++ CursosC#/CFBCursos/Aula 18 - Arrays Bidimensionais (matrizes)/ArraysMatrizes.cs
@@ -13,24 +13,24 @@ namespace CFBCursos.Aula18
             int[,] numeros = new int[3, 5];
 
             numeros[0, 0] = 10;
-            numeros[0, 2] = 20;
-            numeros[0, 3] = 30;
-            numeros[0, 4] = 50;
-            numeros[0, 5] = 60;
+            numeros[0, 1] = 20;
+            numeros[0, 2] = 30;
+            numeros[0, 3] = 50;
+            numeros[0, 4] = 60;
 
             numeros[1, 0] = 70;
-            numeros[1, 2] = 80;
-            numeros[1, 3] = 90;
-            numeros[1, 4] = 100;
-            numeros[1, 5] = 110;
+            numeros[1, 1] = 80;
+            numeros[1, 2] = 90;
+            numeros[1, 3] = 100;
+            numeros[1, 4] = 110;
 
             numeros[2, 0] = 120;
-            numeros[2, 2] = 130;
-            numeros[2, 3] = 140;
-            numeros[2, 4] = 150;
-            numeros[2, 5] = 160;
+            numeros[2, 1] = 130;
+            numeros[2, 2] = 140;
+            numeros[2, 3] = 150;
+            numeros[2, 4] = 160;
 
-            Console.WriteLine(numeros);
+            Console.WriteLine(numeros[0,0]);
         }
     }
     
