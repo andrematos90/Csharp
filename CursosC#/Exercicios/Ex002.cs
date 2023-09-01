2be0a3efb6098b9b4be14bf15bb02b770cc9ebc1
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-09-01 08:12:59 -0300

    Ex 002 - Escopo de Variáveis

---
+++ CursosC#/Exercicios/Ex002.cs
@@ -0,0 +1,33 @@
+﻿/*
+Código em C# com um erro relacionado ao escopo de variáveis para você resolver:
+Seu objetivo é fazer com que o código compile e imprima o valor da variável novoValor. Você precisará ajustar o escopo da variável para corrigir o erro. 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    class Ex002
+    {
+        public static void Executar()
+        {
+
+            int valor = 10;
+            int novoValor = 0; // para resolver o problema se declara e atribui um valor no inicio tornando global
+            
+
+            if (valor > 5)
+            {
+                novoValor = 20;
+            }
+
+            Console.WriteLine(novoValor); // Aqui ocorrerá um erro de compilação
+
+
+        }
+    }
+}
--- CursosC#/Exercicios/Program.cs
+++ CursosC#/Exercicios/Program.cs
@@ -8,6 +8,7 @@ namespace CursoCSharp {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
                 {"Exercício 001 - Média Simples", Ex001.Executar},
+                {"Exercício 002 - Escopo de Variáveis", Ex002.Executar},
             });
 
             central.SelecionarEExecutar();
