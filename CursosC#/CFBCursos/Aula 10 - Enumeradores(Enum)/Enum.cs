7ce7b899fa2285c037e3d8868740f0dcf2207bad
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-09-01 10:38:39 -0300

    Aula 10 - Enumeradores

--- CursosC#/CFBCursos/Aula 10 - Enumeradores(Enum)/Enum.cs
+++ CursosC#/CFBCursos/Aula 10 - Enumeradores(Enum)/Enum.cs
@@ -9,18 +9,23 @@ namespace CFBCursos.Aula10
 {
     class Enumeradores
     {
+        /* DiasSemana é um tipo que possue elementos pré definidos, ele só pode receber esses elementos,
+         cada elemento tem um indice iniciando do 0*/
         enum DiasSemana { Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado };
         public static void Executar()
         {
+            /*variavel "ds" recebe 3 convertido para Diassemana que retorna "quarta"
+             recebe um tipo inteiro e converte para tipo Diassemana
+             */ 
             DiasSemana ds = (DiasSemana)3;
 
-            DiasSemana ds2 = (DiasSemana.Quarta);
+            DiasSemana ds2 = (DiasSemana.Quarta); //variavel "ds" recebe  o elemento diretamente
 
             Console.WriteLine(ds);
             Console.WriteLine(ds2);
 
 
-            int ds3 = (int)DiasSemana.Quarta;
+            int ds3 = (int)DiasSemana.Quarta; // converte tipo DiasSemana em tipo inteiro
 
             Console.WriteLine(ds3); 
 
