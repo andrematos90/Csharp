3b287be3716d52a3930b2521a94fb94fc468ca2b
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-09-07 08:46:54 -0300

    Aula 28 - Classes e Objetos

--- CursosC#/CFBCursos/Aula 28 - Classes e Objetos/ClassesEObjetos.cs
+++ CursosC#/CFBCursos/Aula 28 - Classes e Objetos/ClassesEObjetos.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace CFBCursos.Aula28
 {
+
+    class Jogardor
+    {
+        public int energia = 100;
+        public bool vivo = true;
+    }
+
    class ClassesEObjetos
     {
         public static void Executar()
         {
+            //instanciando criando objetos do tipo "Jogador"
+            Jogardor player1 = new Jogardor();
+            Jogardor player2 = new Jogardor();
+            Jogardor player3 = new Jogardor();
+
+
+            Console.WriteLine(player1.energia); // acessando atribudo do objeto
+            Console.WriteLine(player2.energia = 80); //alterando atributo do objeto
+            Console.WriteLine(player3.vivo);
 
         }
     }
