949dbe4f92dc7b8ea51c3039d4fcd904f58e076e
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-09-15 15:32:22 -0300

    Aula 40 - Classes sealed (seladas)

--- CursosC#/CFBCursos/Aula 40 - Classes Sealed/ClassesSealed.cs
+++ CursosC#/CFBCursos/Aula 40 - Classes Sealed/ClassesSealed.cs
@@ -4,9 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CFBCursos.Aula_40___Classes_Sealed
+namespace CFBCursos.Aula40
 {
-    internal class ClassesSealed
+    sealed class Animal
     {
+      
+    }
+
+    /*public class Cachorro:Animal // gera erro pq a classe Animal é selada
+    {
+
+    }*/
+    class ClassesSealed
+    {
+        public static void Executar()
+        {
+
+        }
     }
 }
